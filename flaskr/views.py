d36e652c1d40916dd783fc92ee99c540f5cd2c3e
Link-yu <kevinyulk@163.com>
2017-07-09 23:21:27 +0800

    opednid

--- flaskr/views.py
+++ flaskr/views.py
@@ -1,14 +1,18 @@
 from flaskr import app
-from flask import render_template
+from flask import render_template, flash, redirect
 from .forms import LoginForm
 
 #index view function suppressed for brevity
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for OpenID="' + form.openid.data + '", remember_me=' +str(form.remember_me.data))
+        return redirect('/index')
     return render_template('login.html',
         title = 'Sigin In',
-        form = form)
+        form = form,
+        providers = app.config['OPENID_PROVIDERS'])
 
 @app.route('/')
 @app.route('/index')
